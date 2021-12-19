c1ae5ef38e8c99e6b3ea3d17c6a8ab6ec4713159
EslamAsfour <40435240+EslamAsfour@users.noreply.github.com>
2021-12-19 20:54:14 +0200

    Added All shortcuts and All the Checks so we dont get an error

--- Form1.cs
+++ Form1.cs
@@ -8,95 +8,91 @@ namespace Bonus
             ch  = new char[] { '*', '+', '-' };
             InitializeComponent();
         }
-
         private void Calc()
         {
             int num1, num2;
-            String FullEx = L_IO.Text;
+            String UncalculatedEq = "",FullEx = L_IO.Text;
             
             char op;
-            int OpInd;
+            int OpInd, OpInd2;
             // Get Op Index
             OpInd = FullEx.IndexOfAny(ch);
             // Get Index Type
             op = FullEx[OpInd];
             // SubString To get Each Number
             num1 = int.Parse(FullEx.Substring(0, OpInd));
-            num2 = int.Parse(FullEx.Substring(OpInd+1));
+            // If We have more Than One Operator So we Take Only the Left Side Op
+            if (FullEx.Substring(OpInd + 1).IndexOfAny(ch) == -1)
+                // No more Operators
+                num2 = int.Parse(FullEx.Substring(OpInd + 1));
+            else
+            // Extract Num2 as the First Number From the Left
+            {
+                OpInd2 = FullEx.Substring(OpInd + 1).IndexOfAny(ch);
+                num2 = int.Parse(FullEx.Substring(OpInd + 1, OpInd2));
+                UncalculatedEq = FullEx.Substring(OpInd + OpInd2 + 1);
+            }
 
-            
             if (op == '+')
-                L_IO.Text = (num1 + num2).ToString();
+                L_IO.Text = (num1 + num2).ToString() + UncalculatedEq;
             else if (op == '-')
-                L_IO.Text = (num1 - num2).ToString();
+                L_IO.Text = (num1 - num2).ToString()+ UncalculatedEq;
             else if (op == '*')
-                L_IO.Text = (num1 * num2).ToString();
+                L_IO.Text = (num1 * num2).ToString()+ UncalculatedEq;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             L_IO.Text += "1";
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             L_IO.Text += "2";
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             L_IO.Text += "3";
         }
-
         private void button6_Click(object sender, EventArgs e)
         {
             L_IO.Text += "4";
         }
-
         private void button5_Click(object sender, EventArgs e)
         {
             L_IO.Text += "5";
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             L_IO.Text += "6";
         }
-
         private void button9_Click(object sender, EventArgs e)
         {
             L_IO.Text += "7";
         }
-
         private void button8_Click(object sender, EventArgs e)
         {
             L_IO.Text += "8";
         }
-
         private void button7_Click(object sender, EventArgs e)
         {
             L_IO.Text += "9";
         }
-
         private void button10_Click(object sender, EventArgs e)
         {
             L_IO.Text += "0";
         }
-
         private void button13_Click(object sender, EventArgs e)
         {
             // If the Last Character is not an operator
             if( ! ch.Contains(L_IO.Text[L_IO.Text.Length - 1]) )
                 L_IO.Text += "+";
         }
-
         private void button12_Click(object sender, EventArgs e)
         {
             // If the Last Character is not an operator
             if (!ch.Contains(L_IO.Text[L_IO.Text.Length - 1]))
                 L_IO.Text += "-";
         }
-
         private void button11_Click(object sender, EventArgs e)
         {            
             // If the Last Character is not an operator
@@ -106,7 +102,9 @@ namespace Bonus
         // Equal
         private void button14_Click(object sender, EventArgs e)
         {
-           Calc();
+            // Check if we Have an Operator 
+            if(L_IO.Text.IndexOfAny(ch) != -1)
+                Calc();
         }
 
         private void button15_Click(object sender, EventArgs e)
@@ -165,12 +163,10 @@ namespace Bonus
                 case '\b':
                     button15.PerformClick();
                     break;
-                    
-
+                case '=':
+                    button14.PerformClick();
+                    break;
             }
         }
-
-
-
     }
 }
